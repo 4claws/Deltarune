abede06169e69d81b4604e945e4f4f4af3e8bc59
xShader1374 <shader1374@gmail.com>
2025-10-14 18:26:38 +0200

    Update javascript.js

--- docs/javascript.js
+++ docs/javascript.js
@@ -19,15 +19,15 @@ function addOstPLayer() {
     `
     <select id="track-selector" style="width: 50%;"></select>
     <div>Trackbar</div>
-    <audio id="audio" src="assets/audio/osts/32%20-%20Toby%20Fox%20-%20Attack%20of%20the%20Killer%20Queen.mp3" equalizer-state="attached"></audio>
+    <audio id="audio" src="https://github.com/4claws/Deltarune/raw/refs/heads/main/docs/assets/audio/osts/1_Beginning.mp3" equalizer-state="attached"></audio>
     `;
     body.appendChild(player);
 
     // List of tracks
     const tracks = [
-        { name: "01 - Beginning", file: "assets/audio/osts/1_Beginning.mp3" },
-        { name: "02 - My Castle Town", file: "assets/audio/osts/1.03%20Toby%20Fox%20-%20DELTARUNE%20Chapter%202%20OST%20-%2003%20My%20Castle%20Town.mp3" },
-        { name: "03 - A CYBER'S WORLD", file: "assets/audio/osts/1.06%20Toby%20Fox%20-%20DELTARUNE%20Chapter%202%20OST%20-%2006%20A%20CYBER'S%20WORLD-.mp3" }
+        { name: "01 - Beginning", file: "https://github.com/4claws/Deltarune/raw/refs/heads/main/docs/assets/audio/osts/1_Beginning.mp3" },
+        { name: "02 - My Castle Town", file: "https://github.com/4claws/Deltarune/raw/refs/heads/main/docs/assets/audio/osts/1.03%20Toby%20Fox%20-%20DELTARUNE%20Chapter%202%20OST%20-%2003%20My%20Castle%20Town.mp3" },
+        { name: "03 - A CYBER'S WORLD", file: "https://github.com/4claws/Deltarune/raw/refs/heads/main/docs/assets/audio/osts/1.06%20Toby%20Fox%20-%20DELTARUNE%20Chapter%202%20OST%20-%2006%20A%20CYBER'S%20WORLD-.mp3" }
     ];
 
     const trackSelector = document.getElementById('track-selector');
