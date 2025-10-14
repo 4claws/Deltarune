4373f51c3c08fef401a9059c24909ba464184369
xShader1374 <shader1374@gmail.com>
2025-10-14 17:45:58 +0200

    update js

--- docs/javascript.js
+++ docs/javascript.js
@@ -8,13 +8,13 @@ document.addEventListener("DOMContentLoaded", function() {
     `
     <select id="track-selector" style="width: 50%;"></select>
     <div>Trackbar</div>
-    <audio id="audio" src="assets/audio/osts/11_Thrash_Machine.mp3" equalizer-state="attached"></audio>
+    <audio id="audio" src="https://github.com/4claws/Deltarune/blob/main/docs/assets/audio/osts/32%20-%20Toby%20Fox%20-%20Attack%20of%20the%20Killer%20Queen.mp3" equalizer-state="attached"></audio>
     `;
     body.appendChild(player);
 
     // List of tracks
     const tracks = [
-        { name: "01 - Beginning", file: "1_Beginning.mp3" },
+        { name: "01 - Beginning", file: "https://github.com/4claws/Deltarune/blob/main/docs/assets/audio/osts/1_Beginning.mp3" },
         { name: "02 - My Castle Town", file: "1.03 Toby Fox - DELTARUNE Chapter 2 OST - 03 My Castle Town.mp3" },
         { name: "03 - A CYBER'S WORLD", file: "1.06 Toby Fox - DELTARUNE Chapter 2 OST - 06 A CYBER'S WORLD-.mp3" }
     ];
