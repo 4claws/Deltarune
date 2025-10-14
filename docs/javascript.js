c7cd4900463025e89a8dd1707bdb3781abd91b9d
xShader1374 <shader1374@gmail.com>
2025-10-14 17:26:58 +0200

    Iniziando a riaggiungere l'ost player

--- docs/javascript.js
+++ docs/javascript.js
@@ -1,1 +1,16 @@
-console.log("Hello, world!");
+// Set the viewport to be responsive
+document.getElementsByName("viewport")[0].setAttribute('content', 'width=device-width, initial-scale=1.0, maximum-scale=1.0');
+
+// Adds a player
+const body = document.body;
+const player = document.createElement('div');
+player.classList.add('ost-player');
+player.id = 'ost-player';
+player.innerHTML = 
+`
+<div class="player">
+    <select id="track-selector" style="width: 50%;"></select>
+    <audio id="audio" src="assets/audio/osts/11_Thrash_Machine.mp3" equalizer-state="attached"></audio>
+</div>
+`;
+body.appendChild(player);
